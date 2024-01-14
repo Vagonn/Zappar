0ff84706db22f2ab9cc0be08509a40566d6cca4b
Nurlan <shukurov.nurlan.1991@gmail.com>
2024-01-14 22:19:10 +0400

    Added project

---
+++ Zappar1/Assets/Test.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Zappar;
+
+public class Test : MonoBehaviour
+{
+    public GameObject greyPanel;
+    public Renderer cube;
+
+    public List<Renderer> parts;
+    public Material mat1;
+    public Material mat2;
+
+    public ZapparInstantTrackingTarget instant;
+
+    public GameObject placeButtonObject;
+    public GameObject resetButtonObject;
+
+    private void Start()
+    {
+        greyPanel.SetActive(false);
+        Mat1();
+    }
+
+    public void PlaceModel()
+    {
+        instant.PlaceTrackerAnchor();
+
+        placeButtonObject.SetActive(false);
+        resetButtonObject.SetActive(true);
+    }
+
+    public void ResetModel()
+    {
+        instant.ResetTrackerAnchor();
+        
+        placeButtonObject.SetActive(true);
+        resetButtonObject.SetActive(false);
+
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Mat1() 
+    {
+        // cube.material = mat1;
+
+        foreach (Renderer renderer in parts)
+        {
+            renderer.material = mat1;
+        }
+    }
+
+    public void Mat2()
+    {
+        // cube.material = mat2;
+
+        foreach (Renderer renderer in parts)
+        {
+            renderer.material = mat2;
+        }
+    }
+}
